6f4e9d5c45f9bdc6db7cdfaece8022f243f6280b
ITLAB <ITLAB@IT>
2017-02-20 16:22:15 +0530

    fifth commit

--- src/com/example/readwrite/MainActivity.java
+++ src/com/example/readwrite/MainActivity.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -13,121 +12,92 @@ import java.io.OutputStreamWriter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {  
-    EditText editTextFileName;
-    EditText editTextData;  
-    Button saveButton;
-    Button readButton;  
-    @Override  
-    protected void onCreate(Bundle savedInstanceState) {  
-        super.onCreate(savedInstanceState);  
-        setContentView(R.layout.activity_main);  
-          
-        editTextFileName=(EditText)findViewById(R.id.editText1);  
-        editTextData=(EditText)findViewById(R.id.editText2);  
-        saveButton=(Button)findViewById(R.id.button1);  
-        readButton=(Button)findViewById(R.id.button2);   
-    saveButton.setOnClickListener(new OnClickListener(){  
-    	  
+public class MainActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         
-        public void onClick(View arg0) {  
-            String filename=editTextFileName.getText().toString();  
-            String data=editTextData.getText().toString();  
-              
-            FileOutputStream fos;  
-               try {  
-                   File myFile = new File("/sdcard/"+filename);  
-                    myFile.createNewFile();  
-                    FileOutputStream fOut = new   
+        final EditText file;
+		final EditText content;
+        Button write,read;
+        
+        file=(EditText)findViewById(R.id.editText1);
+        content=(EditText)findViewById(R.id.editText2);
+        write=(Button)findViewById(R.id.button1);
+        read=(Button)findViewById(R.id.button2);
+        
+        write.setOnClickListener(new OnClickListener(){
 
-FileOutputStream(myFile);  
-                    OutputStreamWriter myOutWriter = new   
-
-OutputStreamWriter(fOut);  
-                    myOutWriter.append(data);  
-                    myOutWriter.close();  
-                    fOut.close();  
-                 
-        Toast.makeText(getApplicationContext(),filename + "saved",Toast.LENGTH_LONG).show();  
-                  
-                 
-               } catch (FileNotFoundException e) {e.printStackTrace();}  
-               catch (IOException e) {e.printStackTrace();}  
-              
-        }  
-          
-    }); 
-    
-
-readButton.setOnClickListener(new OnClickListener(){  
-	  
-    
-    public void onClick(View arg0) {  
-        String filename=editTextFileName.getText().toString();  
-        StringBuffer stringBuffer = new StringBuffer();    
-        String aDataRow = "";  
-        String aBuffer = "";  
-        try {  
-            File myFile = new File("/sdcard/"+filename);  
-            FileInputStream fIn = new FileInputStream(myFile);  
-            BufferedReader myReader = new BufferedReader(  
-                    new InputStreamReader(fIn));  
-              
-            while ((aDataRow = myReader.readLine()) != null) {  
-                aBuffer += aDataRow + "\n";  
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String filename=file.getText().toString();
+				String data=content.getText().toString();
+				
+				  
+                try {  
+                	File sdcard = Environment.getExternalStorageDirectory();
+                	File dir = new File(sdcard.getAbsolutePath() + "/sd/");
+                	dir.mkdir();
+                	File file = new File(dir, "My-File-Name.txt");
+                	FileOutputStream fout= new FileOutputStream(file);
+                    // File myFile = new File("/sdcard/"+filename);  
+                   //  myFile.createNewFile();  
+                   //  FileOutputStream fout = new FileOutputStream(myFile);  
+                     OutputStreamWriter myOutWriter = new OutputStreamWriter(fout);  
+                     myOutWriter.append(data);  
+                     myOutWriter.close();  
+                     fout.close();  
+                     Toast.makeText(getApplicationContext(),filename + " saved",Toast.LENGTH_LONG).show();  
+                } catch (FileNotFoundException e) {e.printStackTrace();
+                Toast.makeText(getApplicationContext(),filename + "not found",Toast.LENGTH_LONG).show();
+                }  
+                catch (IOException e) {e.printStackTrace();
+                Toast.makeText(getApplicationContext(),filename + " IO exception",Toast.LENGTH_LONG).show();
+                } 
+				
+			}
+        	
+        });
+        
+        read.setOnClickListener(new OnClickListener(){  
+        	  
+            @Override  
+            public void onClick(View arg0) {  
+                String filename=file.getText().toString();  
+                StringBuffer stringBuffer = new StringBuffer();    
+                String aDataRow = "";  
+                String aBuffer = "";  
+                try {  
+                	File sdcard = Environment.getExternalStorageDirectory();
+                	File dir = new File(sdcard.getAbsolutePath() + "/sd/");
+                	dir.mkdir();
+                	File file = new File(dir, "My-File-Name.txt");  
+                    FileInputStream fIn = new FileInputStream(file);  
+                    BufferedReader myReader = new BufferedReader(new InputStreamReader(fIn));  
+                    while ((aDataRow = myReader.readLine()) != null){  
+                        aBuffer += aDataRow + "\n";  
+                    }  
+                    myReader.close();  
+                } catch (IOException e){  
+                    e.printStackTrace();  
+                }  
+                Toast.makeText(getApplicationContext(),aBuffer,Toast.LENGTH_LONG).show();  
             }  
-            myReader.close();  
               
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-        Toast.makeText(getApplicationContext  
+        });  
+        		
+    } 
+	     
 
-(),aBuffer,Toast.LENGTH_LONG).show();  
-          
-    }  
-      
-}); 
-    }
 }
-
-
-  
-   /* @Override  
-    public boolean onCreateOptionsMenu(Menu menu) {  
-        // Inflate the menu; this adds items to the action bar if it is present.  
-        getMenuInflater().inflate(R.menu.activity_main, menu);  
-        return true;  
-    }*/  
-  
-
-
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
