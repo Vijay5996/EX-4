c9317e3f34ac443d157ab8516217d81e51d47668
ITLAB <ITLAB@IT>
2017-02-20 15:04:14 +0530

    second commit

--- src/com/example/readwrite/MainActivity.java
+++ src/com/example/readwrite/MainActivity.java
@@ -1,20 +1,55 @@
 package com.example.readwrite;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {  
+    EditText editTextFileName;
+    EditText editTextData;  
+    Button saveButton;
+    Button readButton;  
+    @Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_main);  
+          
+        editTextFileName=(EditText)findViewById(R.id.editText1);  
+        editTextData=(EditText)findViewById(R.id.editText2);  
+        saveButton=(Button)findViewById(R.id.button1);  
+        readButton=(Button)findViewById(R.id.button2);  
+        
+    }  
+  
+   /* @Override  
+    public boolean onCreateOptionsMenu(Menu menu) {  
+        // Inflate the menu; this adds items to the action bar if it is present.  
+        getMenuInflater().inflate(R.menu.activity_main, menu);  
+        return true;  
+    }*/  
+  
+}  
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -31,5 +66,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-}
+    }*/
+
